721f8c67f67a4d61bd59f3479173893dbe230c04
Heng <31088159+shiukaheng@users.noreply.github.com>
2022-07-16 00:28:22 +0800

    empty functions

--- code/deep_features.py
+++ code/deep_features.py
@@ -1,2 +1,17 @@
 # This file will mainly export functions that allow us to interpret textual / image data into vectors that can be fed into umap.
 # It will probably involve either tensorflow and / or sci-kit. We can then call these functions in preprocessing.py so it doesn't get too messy there.
+
+# Image vectorization (via convolutional autoencoder, trained specifically on the M+ dataset)
+
+def encode_image(image):
+    raise NotImplementedError
+
+# Sentence vectorization (via siamese bert)
+
+def encode_sentence(sentence):
+    raise NotImplementedError
+
+# Word vectorization (via word2vec)
+
+def encode_words(word):
+    raise NotImplementedError
--- code/raw_data.py
+++ code/raw_data.py
@@ -1,1 +1,6 @@
 # For combining the scraped web data and data from the M+ dataset into one accessible form
+
+def load_dataset():
+    # Load the M+ dataset into a pandas dataframe and append a new column that includes an image of the artwork.
+    # Potential optimization is to serialize this and load it from a file after it was run once, to reduce later load times (WARNING: over-optimization)
+    raise NotImplementedError
