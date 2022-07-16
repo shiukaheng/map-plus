045e3d7d833f3348b500a00b159639f76a7bc7bc
Heng <31088159+shiukaheng@users.noreply.github.com>
2022-07-16 16:15:05 +0800

    started work on deep_features

--- code/deep_features.py
+++ code/deep_features.py
@@ -1,6 +1,10 @@
 # This file will mainly export functions that allow us to interpret textual / image data into vectors that can be fed into umap.
 # It will probably involve either tensorflow and / or sci-kit. We can then call these functions in preprocessing.py so it doesn't get too messy there.
 
+from sentence_transformers import SentenceTransformer, util
+
+roberta = SentenceTransformer("all-distilroberta-v1")
+
 # Image vectorization (via convolutional autoencoder, trained specifically on the M+ dataset)
 
 def encode_image(image):
@@ -9,7 +13,8 @@ def encode_image(image):
 # Sentence vectorization (via siamese bert)
 
 def encode_sentence(sentence):
-    raise NotImplementedError
+    # Use RoBERTa model from SentenceTransformer
+    pass
 
 # Word vectorization (via word2vec)
 
