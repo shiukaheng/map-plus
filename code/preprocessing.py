afad23c56bf5a88e7152fd23a81712a41189e9a6
HughLi2024 <72953426+HughLi2024@users.noreply.github.com>
2022-07-23 13:28:11 +0800

    Update incomplete preprocessing.py
    - Added functionality to embed constituents information
    - Embedding constituents information does not work yet

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -3,11 +3,13 @@
 
 from typing import Literal
 import pandas as pd
+from pyparsing import col
 import deep_features
 from iteration_utilities import flatten
 from sklearn.preprocessing import normalize
 import numpy as np
 from sklearn.preprocessing import StandardScaler, normalize, MinMaxScaler
+import ast
 
 EMBEDDING_LANGUAGE = Literal["eng", "zh-tc", "averaged"]
 language_suffixes = {
@@ -26,13 +28,17 @@ def preprocess(
 
     expanded_ml_string_features = list(flatten([[f + s for f in ml_string_features] for s in language_suffixes.values()]))
     all_features = numerical_features + expanded_ml_string_features + identifying_features
+    constituent_features = ["name", "roles", "gender", "nationality"]
+
     transformed_objects = pd.DataFrame()
+    transformed_constituents = pd.DataFrame()
 
     # Stage 1: Filter NaN so all the processing works later
     objects = objects.dropna(subset=all_features)
+    constituents = constituents.dropna(subset=constituent_features)
 
     # Stage 2A: Copy numerical features and identifying features
-    for feature in numerical_features + identifying_features:
+    for feature in numerical_features + identifying_features + ['constituents']:
         transformed_objects[feature] = objects[feature]
 
     # Stage 2B: Embed multilingual string features
@@ -46,35 +52,64 @@ def preprocess(
 
     if embedding_language == "averaged":
         raise NotImplementedError("Averaging not implemented yet")
+    
+    print(transformed_objects)
+    print("")
 
     # TODO: Stage 2C: Embed multilang list of string features
 
-    # TODO: Stage 2D: Integrate and embed constituents information
+    # Stage 2D: Integrate and embed constituents information
+    transformed_constituents["id"] = constituents["id"]
+    if embedding_language != "averaged":
+        suffix = language_suffixes[embedding_language]
+        for feature in constituent_features:
+            sentences = constituents[feature].tolist()
+            embeddings = deep_features.encode_sentence(sentences)
+            transformed_constituents[feature+"Embedding"] = list(embeddings)
+    
+    print(transformed_constituents.head(10))
 
-    # Stage 3: Discard identifying features and flatten each row into a single vector
-    flattened = transformed_objects.drop(identifying_features, axis=1).values
-    columns = list(flattened.transpose()) # Transpose to get a list of columns
-    columns = [np.array(c.tolist()) for c in columns] # Fix nested np.arrays, so now all columns are np.arrays
+    for index, row in transformed_objects.head(1).iterrows():
+        # try:
+        row_constituent = ast.literal_eval(row['constituents'])
+        constituent = row_constituent[0]
+        embedding = transformed_constituents.loc[transformed_constituents['id'] == constituent].iloc[:, 1:].values.flatten().tolist()
+
+        transformed_objects["constituentName"] = np.nan
+        transformed_objects['constituentName'].astype('object')
+        transformed_objects.at[index, "constituentName"] = [1,2]
+
+    # except:
+        print(f"Failed to get constituents information for {row['id']}")
+    print("Transformed:")
+    print(transformed_objects.head(10))
+            
+    # # Stage 3: Discard identifying features and flatten each row into a single vector
+    # flattened = transformed_objects.drop(identifying_features, axis=1).values
+    # columns = list(flattened.transpose()) # Transpose to get a list of columns
+    # columns = [np.array(c.tolist()) for c in columns] # Fix nested np.arrays, so now all columns are np.arrays
     
 
-    # Normalize each column
-    normalized_columns = []
-    for c in columns:
-        if c.ndim == 1:
-            normalized_columns.append(
-                normalize(c.reshape(-1, 1), norm='l2', axis=1)
-            )
-        else: # Here we are using dimension > 1 as a proxy that it is an embedding.. not ideal, but it works for now
-            normalized_columns.append(
-                normalize(c, norm='l2', axis=1)
-            )
+    # # Normalize each column
+    # normalized_columns = []
+    # for c in columns:
+    #     if c.ndim == 1:
+    #         normalized_columns.append(
+    #             normalize(c.reshape(-1, 1), norm='l2', axis=1)
+    #         )
+    #     else: # Here we are using dimension > 1 as a proxy that it is an embedding.. not ideal, but it works for now
+    #         normalized_columns.append(
+    #             normalize(c, norm='l2', axis=1)
+    #         )
 
-    # - For numerical features, simply normalize
-    # - For embedded features, perform normalization, then PCA before vectorization
-    #   - Alternatively, perform normalization, then perform PCA after vectorization
-    # - For numerical features, simply normalize
+    # # - For numerical features, simply normalize
+    # # - For embedded features, perform normalization, then PCA before vectorization
+    # #   - Alternatively, perform normalization, then perform PCA after vectorization
+    # # - For numerical features, simply normalize
 
-    # Stage 4: Flatten the columns into a single vector
-    flattened = np.concatenate(normalized_columns, axis=1)
+    # # Stage 4: Flatten the columns into a single vector
+    # flattened = np.concatenate(normalized_columns, axis=1)
 
-    return flattened, objects
+    # return flattened, objects
+
+preprocess(pd.read_csv("../dataset/collection-data-master/objects.csv").head(10), pd.read_csv("../dataset/collection-data-master/constituents.csv").head(300))
